14a0350e0af4eddc73278da88e49d79a528281e4
kev-mwangi <kevinmwas007@gmail.com>
2025-10-21 16:02:36 +0300

    add status

---
+++ script.js
@@ -0,0 +1,6 @@
+const element = document.getElementById('myElement');
+ <div>
+    <input id="newQuoteText" type="text" placeholder="Enter a new quote" />
+    <input id="newQuoteCategory" type="text" placeholder="Enter quote category" />
+    <button onclick="addQuote()">Add Quote</button>
+  </div>
